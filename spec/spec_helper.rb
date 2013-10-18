ddc70d2c0c437789ce97d5c671014ba1373b010e
Robert Schmitt <forever@thelongterm.net>
2013-10-18 01:51:40 -0700

    Added spec helper for run specs.
    Updated dependencies in gemspec for testing

---
+++ spec/spec_helper.rb
@@ -0,0 +1,20 @@
+require 'simplecov'
+if ENV["COV"]
+  SimpleCov.start do
+    add_filter "/spec/"
+    add_filter "/vendor/"
+    add_filter "/config\/initializers/"
+  end
+end
+
+require 'rspec/autorun'
+
+support_dir = File.expand_path(File.dirname(__FILE__) + '/support')
+Dir[File.join(support_dir, '**/*.rb')].each { |f| require f }
+require 'rsqrl'
+
+RSpec.configure do |config|
+  config.extend Macros::Models
+  config.include RspecSupport::UserObject
+
+end
