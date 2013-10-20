0909de43ccff8191bd8b9f2b0035362ad82a08ba
Robert Schmitt <forever@thelongterm.net>
2013-10-20 13:30:18 -0700

    added comments to identity file

--- lib/rsqrl/sqrl/identity.rb
+++ lib/rsqrl/sqrl/identity.rb
@@ -1,9 +1,27 @@
 module Sqrl
+
   class Identity
+
+    ##
+    # master_identity_key - Master Identity Key (256-bits)
+    #
+    # This key is XORed with the result of the password strengthening to create
+    # the original master key
+    #
+    # password_verify- Password Verify Value (128-bits)
+    #
+    # This is the first 128-bits of SHA256 and is used to verify
+    # the password was entered correctly.
+    #
+    # password_salt - Password Salt (64-bits)
+    #
+    # This is a randomly generated salt value generated when the password is
+    # first set. Whenever the password changes, this also should change.
+    ##
 
     # Immutable parameters - only identity_name is optional
     attr_reader :identity_name        # optional
-    attr_reader :master_identity_keys # 256-bits
+    attr_reader :master_identity_key  # 256-bits
     attr_reader :password_verify      # 128-bits
     attr_reader :password_salt        # 64-bits
     attr_reader :password_parameters  # PasswordParameters object
@@ -11,11 +29,11 @@ module Sqrl
     def initialize(options)
       raise ArgumentError if has_bad_parameters?(options)
 
-      @identity_name = options[:identity_name]
-      @master_identity_keys = options[:master_identity_keys]
-      @password_verify = options[:password_verify]
-      @password_salt = options[:password_salt]
-      @password_parameters = options[:password_parameters]
+      @identity_name        = options[:identity_name]
+      @master_identity_key  = options[:master_identity_key]
+      @password_verify      = options[:password_verify]
+      @password_salt        = options[:password_salt]
+      @password_parameters  = options[:password_parameters]
     end
 
     def valid?
@@ -24,11 +42,12 @@ module Sqrl
       validate(password_salt, 8)
     end
 
-    # pack the exported identity into the agreed upon export format, the
+    ##
+    # Pack the exported identity into the agreed upon export format, the
     # current proposal is:
-    #     8-bit signature algorithm version
+    #     8-bit signature algorithm version - currently 1
     #     256-bit encrypted master key
-    #     8-bit password algorithm version
+    #     8-bit password algorithm version - currently 1
     #     64-bit per-password nonce
     #     64-bit per-password verifier
     #     16-bit computation burden spec (10 bit mantissa + 6 bit exp)
@@ -37,8 +56,9 @@ module Sqrl
     #     8-bit SCrypt base-2 exponent of N parameter
     #     8-bit SCrypt r parameter
     #     16-bit SCrypt p parameter
+    ##
     def create_export_package
-
+      "1#{master_identity_key}1#{password_salt}#{password_verify}#{password_parameters.N}#{password_parameters.r}#{shift_bytes(password_parameters.p, 8)}#{shift_bytes(password_parameters.p, 0)}".bytes
     end
 
     def to_s
@@ -55,5 +75,8 @@ module Sqrl
       value.bytesize = number_of_bytes
     end
 
+    def shift_bytes(value, amount)
+      value.bytes >> amount & 0xFF
+    end
   end
 end
